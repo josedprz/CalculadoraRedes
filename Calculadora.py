eba65c8331718335a203d962b56a4646fc631126
Eduardo Pérez <118428508+josedprz@users.noreply.github.com>
2025-02-19 16:25:40 -0600

    Commit Inicial. Se agrega código base de la calculadora.

---
+++ Calculadora.py
@@ -0,0 +1,35 @@
+# Funciones
+
+def calculadora():
+    while True:
+        print("Selecciona una operación: ")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Potencia")
+        print("6. Salir")
+        opcion = int(input("Elige una opción: "))
+        match opcion:
+            case 1:
+                # Suma
+                continue
+            case 2:
+                # Resta
+                continue
+            case 3:
+                # Multiplicación
+                continue
+            case 4:
+                # División
+                continue
+            case 5:
+                # Potencia
+                continue
+            case 6:
+                break
+            case _:
+                print("Opcion no válida")
+
+if __name__ == "__main__":
+    calculadora()
